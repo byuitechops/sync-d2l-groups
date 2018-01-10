5cf0e908af3a1ebd05d0457a62605447f7b27236
Earl <benjam55@byui.edu>
2018-01-10 12:39:49 -0700

    made it a class

--- main.js
+++ main.js
@@ -1,6 +1,37 @@
 const errorLog = []
 
-async function get(url,cb){
+function addErrorLog(message){
+    console.error(message)
+    errorLog.push(message)
+}
+
+class XHR{
+    async function call(method,url,data){
+        return new Promise((resolve,reject) => {
+            var xhttp = new XMLHttpRequest()
+            xhttp.onreadystatechange = function() {
+              if (this.readyState == 4){
+                if(this.status == 200) {
+                    var data = JSON.parse(this.responseText)
+                    resolve(data)
+                } else {
+                    reject(this.status)
+                }
+              }
+            }
+            xhttp.open(method, url, true)
+            data ? xhttp.send(data) : xhttp.send()
+        })
+    }
+    async function get(url){
+        return await this.call("GET",url)
+    }
+    async function post(url,data){
+        return await this.call("POST",url,data)
+    }
+}
+
+async function get(url){
     return new Promise((resolve,reject) => {
         var xhttp = new XMLHttpRequest()
         xhttp.onreadystatechange = function() {
@@ -18,19 +49,18 @@ async function get(url,cb){
     })
 }
 
-function addErrorLog(message){
-    console.error(message)
-    errorLog.push(message)
-}
-
 (async () => {
     const version = 1.9
     const orgUnitId = 10011
+    const xhr = new 
+    // Get all catagories
     var catagories = await get(`/d2l/api/lp/${version}/${orgUnitId}/groupcategories/`)
     
     // catagories[].Name
     // catagories[].GroupCategoryId
     // catagories[].groups[] => groupIds
+    
+    // 
     
     console.log(catagories)
 })()
