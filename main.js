e4972b52358fcca90c00787a060356307ffdd1ee
Earl <benjam55@byui.edu>
2018-01-11 13:57:16 -0700

    made lots of cool changes

--- main.js
+++ main.js
@@ -24,20 +24,23 @@ class XHR{
     }
 }
 
-async function main() {
+async function signStudentUp(signup) {
     const version = 1.9
-    const orgUnitId = 10011
     const xhr = new XHR()
-    // Get all catagories
-    var catagories = await xhr.get(`/d2l/api/lp/${version}/${orgUnitId}/groupcategories/`)
     
+    // Get all catagories
+    var catagories = await xhr.get(`/d2l/api/lp/${version}/${signup.ou}/groupcategories/`)
+    
+    if(catagories.length > 1){
+        signup.error = `${catagories[0].Name} was not the only one, there was also ${catagories.slice(1).reduce((str,c) => str+=c.Name+',','')}`
+    }
     // catagories[].Name
     // catagories[].GroupCategoryId
     // catagories[].groups[] => groupIds
     
-//    await xhr.post(`/d2l/api/lp/${version}/${orgUnitId}/groupcategories/${groupCategoryId}/groups/${groupId}/enrollments/`,{
-//        "UserId": 89239875487
-//    })
+    await xhr.post(`/d2l/api/lp/${version}/${signup.ou}/groupcategories/${groupCategoryId}/groups/${groupId}/enrollments/`,{
+        UserId: userId
+    })
     
     console.log(catagories)
 }
@@ -61,13 +64,24 @@ function parseFile(id){
     })
 }
 
-async function getAllCourses(){
+function startWaiting(){
+    document.getElementById('loading').removeAttribute('hidden')
+    document.querySelectorAll('input').forEach(n => n.setAttribute('disabled',true))
+}
+
+function stopWaiting(){
+    document.getElementById('loading').setAttribute('hidden',true)
+    document.querySelectorAll('input').forEach(n => n.removeAttribute('disabled'))
+}
+
+async function requestAllCourses(){
     var xhr = new XHR()
     var bookmark = null
     var hasMoreItems = true
     var courses = []
+    startWaiting()
     while(hasMoreItems){
-        var data = await xhr.get(`/d2l/api/lp/1.15/enrollments/myenrollments/?orgUnitTypeId=3&Bookmark=${bookmark}`)
+        var data = await xhr.get(`/d2l/api/lp/1.15/enrollments/myenrollments/?orgUnitTypeId=3${bookmark?'&Bookmark='+bookmark:''}`)
         
         bookmark = data.PagingInfo.Bookmark
         hasMoreItems = data.PagingInfo.HasMoreItems
@@ -79,11 +93,51 @@ async function getAllCourses(){
                 name: course.OrgUnit.Name,
             })
         })
+        console.log(bookmark)
     }
-    console.log(courses)
+    stopWaiting()
+    return courses
+}
+
+async function getCourses(){
+    var courses = localStorage.courses
+    if(!courses){
+        courses = await requestAllCourses()
+        localStorage.courses = d3.csvFormat(courses)
+    } else {
+        courses = d3.csvParse(courses)
+    }
+    return courses
+}
+
+function createMap(courses){
+    return courses.reduce((obj,course) => {
+        obj[course.code] = course.id; 
+        return obj
+    },{})
 }
 
 window.onload = async () => {
-    getAllCourses()
-//    var files = await Promise.all([parseFile('studentsCsv')/*,parseFile('mapCsv')*/])
+    var courses = await getCourses()
+    var signups = await parseFile('csv')
+    
+    startWaiting()
+    var courseMap = createMap(courses)
+    
+    for(var i = 0; i < signups.length; i++){
+        signups[0].error = undefined
+        
+        if(!signups[0].REFERENCE){
+            signups[0].error = "Missing the 'REFERENCE' attribute that contains the coruse code"
+            continue;
+        }
+        if(!courseMap[signups[i].REFERENCE]){
+            signups[0].error = "The 'REFERENCE' course code, was not found in my list"
+            continue;
+        }
+        signup.ou = courseMap[signups[i].REFERENCE]
+        await 
+    }
+    console.log(students[0])
+    stopWaiting()
 }
